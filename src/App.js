06fa77852a0aedd493c7b375e4557e9d9593a242
Rose Guttman <roseguttman@gmail.com>
2020-01-29 10:39:42 -0800

    class work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
+import './media.css'
 import NamePicker from './name'
 import {db, useDB} from './db'
-import { FiSend } from "react-icons/fi"
 import { BrowserRouter, Route } from 'react-router-dom'
+import { FiSend, FiCamera } from 'react-icons/fi'
+import Camera from 'react-snap-pic'
+import * as firebase from "firebase/app"
+import "firebase/firestore"
+import "firebase/storage"
+import Div100vh from 'react-div-100vh'
 
 function App(){
   useEffect(()=> {
@@ -19,10 +25,22 @@ function Room(props) {
   const {room} = props.match.params
   const [name, setName] = useState('')
   const messages = useDB(room)
+  const [showCamera, setShowCamera] = useState(false)
+
+  async function takePicture(img){
+    setShowCamera(false)
+    const imgID = Math.random().toString(36).substring(7) //random ID for our image
+    var storageRef = firebase.storage().ref() //Making refrence in storage
+    var ref = storageRef.child(imgID + '.jpg') //creating child of that refrence
+    await ref.putString(img, 'data_url') //uploading image, specifying type
+    db.send({ img: imgID, name, ts: new Date(), room }) //sending image along with everything else
+  }
 
 
- // console.log(messages)
-  return <main>
+
+  return <Div100vh>
+
+  {showCamera && <Camera takePicture={takePicture} />}
 
     <header> 
       <div style={{display:'flex', alignItems:'center'}}>
@@ -33,23 +51,33 @@ function Room(props) {
     </header>
     
     <div className="messages">
-    {messages.map((m,i)=>{
-      return <div key={i} className="messages-wrap" 
-      from={m.name===name?'me':'you'}>
-        <div className="message">
-            <div className="msg-name">{m.name}</div>
-            <div className="msg-text">{m.text}</div>
-        </div>
-      </div>
-    })}
+    {messages.map((m,i)=> <Message key={i} m={m}  name = {name}/>)}
     </div>
 
-    <TextInput onSend={(text)=> {
+    <TextInput 
+      showCamera={()=>setShowCamera(true)}
+      onSend={(text)=> {
       db.send({
         text, name, ts: new Date(), room
-      })}}/>
+      })}}
+      />
 
-  </main>
+  </Div100vh>
+
+    }
+
+    const bucket = 'https://firebasestorage.googleapis.com/v0/b/chitchat2020-f0147.appspot.com/o/'
+    const suffix = '.jpg?alt=media'
+  
+function Message({m, name}){
+  return <div className="messages-wrap" 
+  from={m.name===name?'me':'you'}>
+    <div className="message">
+        <div className="msg-name">{m.name}</div>
+        <div className="msg-text">{m.text}</div>
+        {m.img && <img src={bucket+m.img+suffix} alt="pic" /> }
+    </div>
+  </div>
 }
 
 
@@ -57,29 +85,35 @@ function TextInput(props){
   const [text, setText] = useState('')
   
   return <div className="text-input-wrap">
-  <div className="bar">
-  <input 
-    className="text-input" value={text}
-    placeholder="Chat"
-    onChange={e=> setText(e.target.value)}
-    onKeyPress={e=> {
-      if(e.key==='Enter') {
-        if(text) props.onSend(test)
-        setText('')
+    <button onClick={props.showCamera}
+      style={{position:'absolute', left: 2, top: 10, background:'transparent',
+      color:'white', height: 29, padding: 4}}>
+      <FiCamera style={{height:15, width:15}} />
+    </button>
+    
+    <input 
+      className="text-input" value={text}
+      placeholder="Chat"
+      onChange={e=> setText(e.target.value)}
+      onKeyPress={e=> {
+        if(e.key==='Enter') {
+          if(text) props.onSend(text)
+          setText('')
       }
     }} />
   
-  <button 
-    onClick={()=> {
-    if(text) props.onSend(text)
-    setText('')
-    }}
-    className="button"
-    disabled={!text}>
-    <FiSend/>
-  </button>
-  </div>
+    <button 
+      onClick={()=> {
+      if(text) props.onSend(text)
+      setText('')
+      }}
+      className="button"
+      disabled={!text}>
+      <FiSend/>
+    </button>
   </div>
 }
+
+
 
 export default App;
